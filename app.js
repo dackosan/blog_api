ef7580ce968d80798dd645d569699a3062b57e55
Gyorgy David <gyorgy.david2006@gmail.com>
2025-10-07 12:20:26 +0200

    routes

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
+import postRoutes from "./routers/postsRoutes.js";
+import usersRouter from "./routers/usersRoutes.js";
 
 const PORT = 3001;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+app.use("/posts", postRoutes);
+app.use("/users", usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Server runs on ${PORT}`);
