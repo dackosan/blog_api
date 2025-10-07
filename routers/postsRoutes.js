2f5edc2095d1ac9841f31e7c00705bcac8788c87
Gyorgy David <gyorgy.david2006@gmail.com>
2025-10-07 12:31:48 +0200

    post endpoints

--- routers/postsRoutes.js
+++ routers/postsRoutes.js
@@ -3,8 +3,23 @@ import * as Post from "../data/post.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("posts");
+router.get("/posts", (req, res) => {
+  res.status(200).json(Post.getPosts());
 });
+
+router.get("/posts/:id", (req, res) => {
+  const post = Post.getPostById(+req.params.id);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found!" });
+  }
+
+  res.status(200).json(post);
+});
+
+router.post("/posts", (req, res) => {});
+
+router.put("/posts/:id", (req, res) => {});
+
+router.delete("/posts/:id", (req, res) => {});
 
 export default router;
