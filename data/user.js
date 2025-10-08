33ce0479b125a758f14cc116987f2dcd0d6f7166
Gyorgy David <gyorgy.david2006@gmail.com>
2025-10-08 12:12:57 +0200

    user done

--- data/user.js
+++ data/user.js
@@ -3,3 +3,19 @@ import db from "./data.js";
 db.prepare(
   "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING, email STRING, password STRING)"
 ).run();
+
+export const getUsers = () => db.prepare("SELECT * FROM users").all();
+export const getUserById = (id) =>
+  db.prepare("SELECT * FROM users WHERE id = ?").get(id);
+export const getUserByEmail = (email) =>
+  db.prepare("SELECT * FROM users WHERE email = ?").get(email);
+export const saveUser = (name, email, password) =>
+  db
+    .prepare("INSERT INTO users (name, email, password) VALUES (?,?,?)")
+    .run(name, email, password);
+export const updateUser = (id, name, email, password) =>
+  db
+    .prepare("UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?")
+    .run(name, email, password, id);
+export const deleteUser = (id) =>
+  db.prepare("DELETE FROM users WHERE id = ?").run(id);
--- routers/usersRoutes.js
+++ routers/usersRoutes.js
@@ -1,10 +1,67 @@
 import { Router } from "express";
 import * as User from "../data/user.js";
+import bcrypt from "bcrypt";
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("users");
+  res.status(200).json(User.getUsers());
+});
+
+router.get("/:id", (req, res) => {
+  const user = User.getUserById(+req.params.id);
+  if (!user) {
+    return res.status(404).json({ message: "User not found!" });
+  }
+
+  res.status(200).json(user);
+});
+
+router.post("/", async (req, res) => {
+  const { name, email, password } = req.body;
+  if ((!name || !email, !password)) {
+    return res.status(403).json({ message: "Invalid credentials!" });
+  }
+
+  const user = User.getUserByEmail(email);
+  if (!user) {
+    const salt = await bcrypt.genSalt();
+    const hashedPass = await bcrypt.hash(password, salt);
+    User.saveUser(name, email, hashedPass);
+
+    return res.status(201).json({ message: "User created!" });
+  }
+
+  res.status(409).json({ message: "Email already used!" });
+});
+
+router.put("/:id", async (req, res) => {
+  const user = User.getUserById(+req.params.id);
+  if (!user) {
+    return res.status(404).json({ message: "User not found!" });
+  }
+
+  const { name, email, password } = req.body;
+  if ((!name || !email, !password)) {
+    return res.status(403).json({ message: "Invalid credentials!" });
+  }
+
+  const salt = await bcrypt.genSalt();
+  const hashedPass = await bcrypt.hash(password, salt);
+  User.updateUser(user.id, name, email, hashedPass);
+
+  return res.status(204).json({ message: "User updated!" });
+});
+
+router.delete("/:id", (req, res) => {
+  const user = User.getUserById(+req.params.id);
+  if (!user) {
+    return res.status(404).json({ message: "User not found!" });
+  }
+
+  User.deleteUser(user.id);
+
+  return res.status(200).json({ message: "User deleted!" });
 });
 
 export default router;
