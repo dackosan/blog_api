ff2e8b5f3a53d302e24334be6e525a6ab28eed84
Gyorgy David <gyorgy.david2006@gmail.com>
2025-10-15 11:36:08 +0200

    jwt

--- routers/postsRoutes.js
+++ routers/postsRoutes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import * as Post from "../data/post.js";
 import * as User from "../data/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -59,5 +61,40 @@ router.delete("/:id", (req, res) => {
 
   return res.status(200).json({ message: "Post deleted!" });
 });
+
+//login
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Invalid credentials!" });
+  }
+
+  const user = User.getUserByEmail(email);
+  if (!user) {
+    return res.status(404).json({ message: "User not found!" });
+  }
+
+  if (!bcrypt.compareSync(password, user.password)) {
+    return res.status(400).json({ message: "Invalid password!" });
+  }
+
+  const token = jwt.sign({ id: user.id, email: user.email }, "secret_key", {
+    expiresIn: "30m",
+  });
+
+  res.json({ token: token });
+});
+
+router.get("/my", (req, res) => {});
+
+function auth(req, res, next) {
+  const accessToken = req.header.authorize;
+  if (!accessToken) {
+    return res.status(401).json({ message: "Unauthorized!" });
+  }
+
+  console.log(accessToken);
+  const token = jwt.verify(accessToken, "secret_key");
+}
 
 export default router;
